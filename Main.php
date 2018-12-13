a64216b70055fce980a5638b854e59d4dce4396c
filippofinke <filippo.finke@samtrevano.ch>
2018-12-13 15:45:03 +0100

    Modified settings

--- Main.php
+++ Main.php
@@ -1,10 +1,12 @@
 <?php
-namespace Classes;
+use Classes\KioskAPI;
+use Classes\Logger;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $settings["app"]["signature"] = "ciao";
 $settings["app"]["deviceuuid"] = "ciao";
-$settings["logger"]["display"] = 1;
+$settings["logger"]["path"] = "tmp/KioskAPI.log";
+$settings["logger"]["display"] = Logger::VISIBLE;
 
 $ka = new KioskAPI($settings);
