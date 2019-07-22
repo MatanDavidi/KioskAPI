836e34977018a26879e0c48c0a6f33f9bed3576f
Filippo Finke <filippo.finke@ComuneChiasso.Locale>
2019-07-22 16:33:00 +0200

    Implemented most of the endpoints, also create a simple example

--- Classes/Api.php
+++ Classes/Api.php
@@ -17,26 +17,105 @@ class Api {
         $this->logger->log("Api initialized with device uuid: " . $this->uuid );
     }
 
-    public function getLanguages() {
-        $signatureinfo = Signature::getSignature('');
-        $signature = $signatureinfo["signature"];
-        $timestamp = $signatureinfo["date"];
+    private function request($endpoint, $signature, $post = null, $put = false) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->baseUrl.'v1/Languages?');
+
+        curl_setopt($ch, CURLOPT_URL, $this->baseUrl.$endpoint);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+        if($post != null)
+        {
+            $post = json_encode($post);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            if($put) {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            } else {
+                curl_setopt($ch, CURLOPT_POST, 1);
+            }
+        }
+        
         $headers = array();
         $headers[] = 'Host: kkiosk-ufeed.feeds.barcodes.no';
         $headers[] = 'Accept: */*';
         $headers[] = 'Content-Type: application/json';
-        $headers[] = 'X-Liquid-Timestamp: '.$timestamp;
-        $headers[] = 'User-Agent: K-Kiosk/437 CFNetwork/975.0.3 Darwin/18.2.0';
-        $headers[] = 'X-Liquid-Signature: '.$signature;
         $headers[] = 'Accept-Language: de-ch';
+        $headers[] = 'X-Liquid-Timestamp: '.$signature["date"];
+        $headers[] = 'User-Agent: K-Kiosk/437 CFNetwork/975.0.3 Darwin/18.2.0';
+        $headers[] = 'X-Liquid-Signature: '.$signature["signature"];
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
         $result = curl_exec($ch);
         curl_close($ch);
+
+        $this->logger->log( __CLASS__ . " " . __FUNCTION__ . " " . $result);
         return json_decode($result, true);
+    }
+
+    public function getLanguages() {
+        $signature = Signature::getSignature('');
+        $this->logger->log( __CLASS__ . " " . __FUNCTION__);
+        $result = $this->request("v1/Languages?", $signature);
+        return $result;
+    }
+
+    public function initialize() {
+        $signature = Signature::getSignature($this->uuid);
+        $post = array(
+            "Culture" => "de-CH",
+            "DeviceId" => $this->uuid
+        );
+        $this->logger->log( __CLASS__ . " " . __FUNCTION__);
+        $result = $this->request("v1/initialize", $signature, $post);
+        return $result;
+    }
+
+    public function requestPin($userId, $phone) {
+        $signature = Signature::getSignature($userId.$phone);
+        $post = array(
+            "MSN" => $phone,
+            "UserID" => $userId
+        );
+        $this->logger->log( __CLASS__ . " " . __FUNCTION__);
+        $result = $this->request("v1/pin", $signature, $post);
+        return $result;
+    }
+
+    public function register($userId, $pin, $birthdate) {
+        $signature = Signature::getSignature($userId.$pin);
+        $post = array(
+            "DateOfBirth" => $birthdate,
+            "Pin" => $pin,
+            "ApprovedConsents" => [543],
+            "RevokedConsents" => [],
+            "UserID" => $userId
+        );
+        $this->logger->log( __CLASS__ . " " . __FUNCTION__);
+        $result = $this->request("v1/user", $signature, $post);
+        return $result;
+    }
+
+    public function registerUser($userId, $name, $gender, $birthdate, $culture) {
+        $signature = Signature::getSignature($userId);
+        $post = array(
+            "DateOfBirth" => $birthdate,
+            "Culture" => $culture,
+            "Name" => $name,
+            "Gender" => $gender,
+            "ApprovedConsents" => [543],
+            "RevokedConsents" => [502,837],
+            "UserID" => $userId
+        );
+        $this->logger->log( __CLASS__ . " " . __FUNCTION__);
+        $result = $this->request("v1/user", $signature, $post, true);
+        return $result;
+    }
+
+    public function getCoupons($userId) {
+        $signature = Signature::getSignature($userId);
+        $this->logger->log( __CLASS__ . " " . __FUNCTION__);
+        $result = $this->request("v1/content?UserID=".$userId, $signature);
+        return $result;
     }
 
 }
--- Main.php
+++ Main.php
@@ -11,6 +11,30 @@ $settings["logger"]["path"] = "tmp/KioskAPI.log";
 $settings["logger"]["display"] = Logger::VISIBLE;
 
 $ka = new KioskAPI($settings);
-$languages = $ka->api->getLanguages();
 
+$languages = $ka->api->getLanguages();
 var_dump($languages);
+
+$initialize = $ka->api->initialize();
+var_dump($initialize);
+
+$tempUserId = $initialize["UserId"];
+
+$pin = $ka->api->requestPin($tempUserId, "Phone number: ");
+
+var_dump($pin);
+
+$register["ResponseStatus"]["ErrorCode"] = "12";
+while($register["ResponseStatus"]["ErrorCode"] == 12)
+{
+    $register = $ka->api->register($tempUserId, readLine("Pin: "), "1980-01-01");
+    var_dump($register);
+}
+var_dump($register);
+
+$userId = $register["UserId"];
+$updated = $ka->api->registerUser($userId, "Test Test", "M", "1980-01-01", "de-CH");
+var_dump($updated);
+
+$coupons = $ka->api->getCoupons($userId);
+var_dump($coupons);
