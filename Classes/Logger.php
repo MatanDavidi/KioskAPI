1465e0bf205274d5f2ace40b8421f0295d5bfa9d
Filippo Finke <filippo.finke@samtrevano.ch>
2019-07-22 19:52:25 +0200

    Little fix

---
+++ Classes/Logger.php
@@ -0,0 +1,23 @@
+<?php
+namespace Classes;
+class Logger
+{
+    private $logFile;
+    private $display;
+    const HIDDEN = 0;
+    const VISIBLE = 1;
+    public function __construct($logFile, $display = 0)
+    {
+        $this->logFile = $logFile;
+        $this->display = $display;
+    }
+    public function log($text)
+    {
+        $date = date("[H:i:s d/m/y]", time());
+        $text = $date." ".$text."\n";
+        if ($this->display == self::VISIBLE) {
+            echo $text;
+        }
+        file_put_contents($this->logFile, $text, FILE_APPEND);
+    }
+}
